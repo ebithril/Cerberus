d4682bfc1aabdf97560c5d7dd936539d7aa1b19d
Patrik Görling <p.gorling@gmail.com>
2017-08-12 01:31:40 +0200

    Prepared file functions to make an easy switch to SDL file handling in the future

--- Utils/FileOperations.cpp
+++ Utils/FileOperations.cpp
@@ -3,16 +3,40 @@
 #include <stdio.h>
 #include "UString.h"
 
+typedef FILE FileHandle;
+
+FileHandle* OpenFile(const String& FileName, const String& Mode)
+{
+	return fopen(FileName.CString(), Mode.CString());
+}
+
+void CloseFile(FileHandle* Handle)
+{
+	fclose(Handle);
+}
+
+int32 GetFileSize(FileHandle* Handle)
+{
+	fseek(Handle, 0L, SEEK_END);
+	const int32 fileSize = ftell(Handle);
+	fseek(Handle, 0L, SEEK_SET);
+	return fileSize;
+}
+
+void ReadFromFile(void* dst, int32 size, int32 count, FileHandle* Handle)
+{
+	fread(dst, size, count, Handle);	
+}
+
 void ReadFileIntoArray(const String& FileName, Array<int8>& outArray)
 {
-	FILE* file = fopen(FileName.CString(), "r");
+	FileHandle* file = OpenFile(FileName, "r");
 
-	fseek(file, 0L, SEEK_END);
-	int32 fileSize = ftell(file);
-	fseek(file, 0L, SEEK_SET);
+	const int32 fileSize = GetFileSize(file);
 
 	outArray.InitEmpty(fileSize);
-	fread((void*)outArray.Data(), sizeof(int8), fileSize, file);
+	ReadFromFile((void*)outArray.Data(), sizeof(int8), fileSize, file);
 
-	fclose(file);
+	CloseFile(file);
 }
+
