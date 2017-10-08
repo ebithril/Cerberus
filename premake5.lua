6f908845740de40873b9fb0d246802429ad55311
Patrik Görling <p.gorling@gmail.com>
2017-10-08 17:12:04 +0200

    Updated premake script

--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,16 @@
 workspace "Cerberus"
 	configurations { "Debug", "Release" }
+	filter "language:C++"
+		buildoptions "-std=c++11"
 
 project "Cerberus"
 	kind "ConsoleApp"
 	language "C++"
 	targetdir "bin/%{cfg.buildcfg}"
 
-	files { "**.h", "**.c", "**.cpp" }
+	files { "*.h", "*.c", "*.cpp" }
+	includedirs { "Utils", "Engine" }
+	links { "Engine", "Utils" }
 
 	filter "configurations:Debug"
 		defines { "DEBUG" }
@@ -15,3 +19,36 @@ project "Cerberus"
 	filter "configurations:Release"
 		defines { "NDEBUG" }
 		optimize "On"
+
+project "Engine"
+	kind "StaticLib"
+	language "C++"
+	
+	files { "Engine/**.h", "Engine/**.c", "Engine/**.cpp" }
+	includedirs { "Utils", "C:/VulkanSDK/1.0.46.0/Include" }
+	libdirs { os.findlib("Vulkan") }
+	links { "Utils" }
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
+project "Utils"
+	kind "StaticLib"
+	language "C++"
+	
+	files { "Utils/**.h", "Utils/**.c", "Utils/**.cpp" }
+	includedirs { "thirdparty" }
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
